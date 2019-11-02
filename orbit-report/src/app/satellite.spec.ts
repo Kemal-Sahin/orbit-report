e32f338c9f06d12903476b9199951cd07e6b0cd2
unknown <ksahin4@gmail.com>
2019-11-01 21:55:30 -0500

    done!

---
+++ orbit-report/src/app/satellite.spec.ts
@@ -0,0 +1,8 @@
+import { Satellite } from './satellite';
+import { AppComponent } from './app.component';
+
+describe('Satellite', () => {
+  it('should create an instance', () => {
+    expect(new Satellite()).toBeTruthy();
+  });
+});
